f869b5ea22edf86f28f1f41c0c952ed51590dc18
sn0wyQ <soezforkm@gmail.com>
2023-02-08 23:31:34 +0100

    now supporting ENV expressions to be inside of strings

--- src/deployUtils.ts
+++ src/deployUtils.ts
@@ -1,5 +1,28 @@
 import fs from 'fs';
 
+function replaceEnvExpressions(str: string): string {
+    let result = "";
+    let curStrIdx = 0;
+    let envExprStartIdx = str.indexOf("ENV{{");
+
+    do {
+        let envExprEndIdx = str.indexOf("}}", envExprStartIdx);
+        if (envExprEndIdx != -1) {
+            result += str.substring(curStrIdx, envExprStartIdx);
+            const envName = str.substring(envExprStartIdx + 5 /* "ENV{{" */, envExprEndIdx).trim();
+            result += process.env[envName];
+            curStrIdx = envExprEndIdx + 2; // "}}"
+        } else {
+            break;
+        }
+    } while (envExprStartIdx != -1);
+
+    if (curStrIdx < result.length) {
+        result += str.substring(curStrIdx);
+    }
+    return result;
+}
+
 /**
  * Loads json config, replacing expressions with evaluated values:
  * - ENV{{ ENV_NAME }} => process.env.ENV_NAME
@@ -8,16 +31,15 @@ import fs from 'fs';
  * @param configPath Path to config file.
  * @param errCallback Function to be called on error.
  */
-export function loadConfigWithExperssions(
+export function loadConfigWithExpressions(
     configPath: fs.PathOrFileDescriptor, errCallback?: (err: any) => void): any {
     try {
         const rawConfigJson = fs.readFileSync(configPath, {encoding:'utf8', flag:'r'});
         return JSON.parse(rawConfigJson, (key: string, value: any) => {
-            if (typeof value === 'string' || value instanceof String) {
-                if (value.startsWith("ENV{{") && value.endsWith("}}")) {  // ENV variable
-                    const env_name = value.substring(5, value.length - 2).trim();
-                    return process.env[env_name];
-                }
+            if (typeof value === 'string') {
+                let result = replaceEnvExpressions(value);
+                // TODO: Add more possible expressions
+                return result;
             }
             return value;
         });
